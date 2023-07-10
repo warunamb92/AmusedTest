8efee4ee6922933a194ffb2dd138fe3b5b5a223e
John Doe <waruna.ism@gmail.com>
2023-07-10 20:16:49 +0530

    Get All the Objects

--- cypress/e2e/GetMethodAPIs.cy.js
+++ cypress/e2e/GetMethodAPIs.cy.js
@@ -1,6 +1,21 @@
 /// <reference types="cypress" />
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+//Retrive object data from the test API
+describe('Retrive Data from the Amused Test API',function() {
+
+  beforeEach(function() {
+    cy.defineApiEndpoint(); //call the end point command 
+  });
+
+
+
+  it('Get All Objects', function() { 
+    cy.request({
+      method: 'GET',
+      url: Cypress.env('apiEndpoint') // call env endpoint url in the command of defineApiEndpoint
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+    
+    });
+})
+
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,9 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+Cypress.Commands.add('defineApiEndpoint', function() {
+    Cypress.env('apiEndpoint', 'https://api.restful-api.dev/objects');
+   
+  });
