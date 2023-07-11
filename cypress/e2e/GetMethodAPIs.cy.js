0d71d9525a50f9f66bf16cbecb79a26557f4d44a
John Doe <waruna.ism@gmail.com>
2023-07-11 20:04:23 +0530

    Setup the fixture for one  validation

--- cypress/e2e/GetMethodAPIs.cy.js
+++ cypress/e2e/GetMethodAPIs.cy.js
@@ -2,27 +2,37 @@
 //Retrive object data from the test API
 describe('Retrive Data from the Amused Test API', function () {
 
+  before(function(){
+    cy.fixture('/API_data.json').then(function (APIResponseData) {         // call fixture file APIData Json 
+
+      this.APIResponseData = APIResponseData;
+    })
+  })
+
   beforeEach(function () {
-    cy.defineApiEndpoint(); //call the end point command 
+    cy.defineApiEndpoint(); //call the end point command ----Command.js 
   });
+
+
   it('Get All Objects-Check Status Code', function () {
     cy.request({
       method: 'GET',
       url: Cypress.env('apiEndpoint') // call env endpoint url in the command of defineApiEndpoint
     }).then((response) => {
-      expect(response.status).to.eq(200); // verify the status code is 200
+      // verify the status code is 200 
+      expect(response.status).to.eq(200); 
 
       //validate response body 
       const _response = response.body; // store response body into the variable
       expect(_response).to.have.length(13);
 
-      // Validate properties and values of specific objects
+      // Validate properties and values of specific objects-------------------------------this has been done using the fixture file called API_data json file 
       const _firstObject = _response[0]; // taking the first object and read the data
-      expect(_firstObject).to.have.property('id', '1');
-      expect(_firstObject).to.have.property('name', 'Google Pixel 6 Pro');
+      expect(_firstObject).to.have.property('id',this.APIResponseData.id);
+      expect(_firstObject).to.have.property('name',this.APIResponseData.name);
       expect(_firstObject.data).to.deep.equal({
-        color: 'Cloudy White',
-        capacity: '128 GB'
+        color: this.APIResponseData.color,
+        capacity: this.APIResponseData.capacity
       });
 
       // Validate properties and values of second objects
